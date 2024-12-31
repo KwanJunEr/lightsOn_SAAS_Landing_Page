f6d182c6d6197cb273103611be6700d6e6bc5a2c
KwanJunEr <jonastheitguy@gmail.com>
2024-12-31 18:34:54 +0800

    completed the product showcase component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/sections/Hero";
 import { Header } from "@/sections/Header";
 import LogoTicker from "@/sections/LogoTicker";
+import ProductShowcase from "@/sections/ProductShowcase";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <Header/>
     <Hero/>
     <LogoTicker/>
+    <ProductShowcase/>
    </>
   );
 }
